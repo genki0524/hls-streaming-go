9c554ec370d86e8600de3616c675f1a7041a481e
genki <fujigen20020809@icloud.com>
2026-02-06 18:52:40 +0900

    update: mdeia.goでStorageRepositoryを利用していたが、直接GCSRepositoryを利用

--- hls-streaming/internal/service/media.go
+++ hls-streaming/internal/service/media.go
@@ -7,16 +7,16 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/genki0524/hls_striming_go/internal/domain/repository"
+	"github.com/genki0524/hls_striming_go/internal/infrastructure"
 	"github.com/genki0524/hls_striming_go/internal/media"
 )
 
 type MediaService struct {
-	storage       repository.StorageRepository
+	storage       *infrastructure.GCSRepository
 	ffmpegService *media.FFmpegService
 }
 
-func NewMediaService(storage repository.StorageRepository, ffmpegService *media.FFmpegService) *MediaService {
+func NewMediaService(storage *infrastructure.GCSRepository, ffmpegService *media.FFmpegService) *MediaService {
 	return &MediaService{
 		storage:       storage,
 		ffmpegService: ffmpegService,
